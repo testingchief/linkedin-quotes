c51a879e49f0551d54acdf66804af0b956ce243d
skthetester <skthetester@gmail.com>
2023-09-30 12:26:37 -0400

    Updated tests

--- tests/conf_test.bat
+++ tests/conf_test.bat
@@ -1,8 +1,4 @@
-py .\scripts\linkedin-quotes.py "urn:li:share:7112420626157510656" "001" 
-py .\scripts\linkedin-quotes.py "urn:li:share:7107345645132251138" "002" 
-py .\scripts\linkedin-quotes.py "urn:li:share:7105527122726699008" "003" 
-py .\scripts\linkedin-quotes.py "urn:li:share:6966754218011504640" "004" 
-py .\scripts\linkedin-quotes.py "urn:li:ugcPost:71074517839746457" "005" 
-py .\scripts\linkedin-quotes.py "urn:li:share:7108659246350639104" "006" 
-py .\scripts\linkedin-quotes.py "urn:li:share:7110123670982860801" "007" 
+py .\scripts\linkedin-quotes.py "urn:li:share:7037645630600617984" "blue" 
+py .\scripts\linkedin-quotes.py "urn:li:share:6966754218011504640" "" 
+py .\scripts\linkedin-quotes.py "urn:li:share:7110123670982860801" "yellow" 
 pause 
